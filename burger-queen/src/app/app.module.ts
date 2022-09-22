01078e32ef8eb951a282be769a8fd9714c8f3fc4
VanezaCT <cryse.835@gmail.com>
2022-09-21 22:43:05 -0500

    instaldo angular y creado componentes

---
+++ burger-queen/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './plantilla/header/header.component';
+import { FooterComponent } from './plantilla/footer/footer.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+import { ModelosComponent } from './modelos/modelos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NuevoComponent,
+    EditarComponent,
+    ModelosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
