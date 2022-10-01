77eeb7235904eff0a636aeb6ad99c75d4e94e77b
LeylaSolange <leylagamarra.movistar@gmail.com>
2022-10-01 02:23:26 -0500

    data json

--- burger-queen/src/app/vistas/mesero/mesero.component.ts
+++ burger-queen/src/app/vistas/mesero/mesero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import productsSnk from "src/assets/data/appiBurguer.json";
+//import productsSnk from "src/assets/data/appiBurguer.json";
+import { HttpClient } from '@angular/common/http'; 
+import * as data from '../../../assets/data/appiBurguer.json';
 
 @Component({
   selector: 'app-mesero',
@@ -7,13 +9,19 @@ import productsSnk from "src/assets/data/appiBurguer.json";
   styleUrls: ['./mesero.component.css']
 })
 export class MeseroComponent implements OnInit {
+  public contentfile: string ="";
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     
 }
-productos:any = productsSnk.products
+//productos:any = productsSnk.products
 
   ngOnInit(): void {
+  }
+  public mostrarJson(): void{
+    this.httpClient.get("assets/data/appiBurguer.json").subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
