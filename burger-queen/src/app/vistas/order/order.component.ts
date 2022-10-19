520627e4afd5b2108643d53c618121a7cbe3b448
LeylaSolange <leylagamarra.movistar@gmail.com>
2022-10-19 18:20:15 -0500

    Leyla

--- burger-queen/src/app/vistas/order/order.component.ts
+++ burger-queen/src/app/vistas/order/order.component.ts
@@ -66,7 +66,7 @@ export class OrderComponent implements OnInit {
        
       this.total=this.arrsubTotal.reduce((a: any,b: any) => { return a+b})
       console.log(this.total)
-      console.log(this.total)
+      
 
 
 
